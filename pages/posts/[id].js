8d5d2997552995d1fa6d728c1645b4b4583bbfbf
JangYoungJu <ju7435@naver.com>
2022-11-06 00:16:42 +0900

    Dynamic Import 사용해보기

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -7,6 +7,12 @@ import utilStyles from '/styles/utils.module.css';
 import { MDXRemote } from 'next-mdx-remote';
 import CodeBlock from 'components/CodeBlock';
 import { useRouter } from 'next/router';
+// import Button from '../../components/Button';
+import dynamic from 'next/dynamic';
+
+const Button = dynamic(() => import('../../components/Button'), {
+    loading: () => <div>Loading...</div>,
+});
 
 export async function getStaticPaths() {
     const paths = getAllPostIds();
@@ -32,17 +38,6 @@ export async function getStaticProps({ params, preview }) {
         },
     };
 }
-
-const Button = ({ children }) => {
-    return (
-        <button
-            className="bg-black dark:bg-white text-lg text-teal-200 dark:text-teal-700 rounded-lg px-5"
-            onClick={() => alert(`thanks to ${children}`)}
-        >
-            {children}
-        </button>
-    );
-};
 
 const components = {
     Button,
